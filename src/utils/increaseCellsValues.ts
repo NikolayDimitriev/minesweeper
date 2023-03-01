4ae555a14dd8ef8cbed96b37c01ced4be195e6d8
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-03-01 23:46:20 +0300

    разбил на отдельные функции увеличение значений клеток

--- src/utils/increaseCellsValues.ts
+++ src/utils/increaseCellsValues.ts
@@ -4,53 +4,84 @@ export function increaseCellsValuesAroundMine(
   j: number
 ) {
   if (i === 0 && j === 0) {
-    matrix[i + 1][j] += 1;
-    matrix[i][j + 1] += 1;
-    matrix[i + 1][j + 1] += 1;
+    increaseRightDownTriangle(matrix, i, j);
   } else if (i === 15 && j === 15) {
-    matrix[i - 1][j] += 1;
-    matrix[i][j - 1] += 1;
-    matrix[i - 1][j - 1] += 1;
+    increaseLeftUpTriangle(matrix, i, j);
   } else if (i === 0 && j === 15) {
-    matrix[i + 1][j] += 1;
-    matrix[i][j - 1] += 1;
-    matrix[i + 1][j - 1] += 1;
+    increaseLeftDownTriangle(matrix, i, j);
   } else if (i === 15 && j === 0) {
-    matrix[i - 1][j] += 1;
-    matrix[i][j + 1] += 1;
-    matrix[i - 1][j + 1] += 1;
+    increaseRightUpTriangle(matrix, i, j);
   } else if (i === 0) {
-    matrix[i + 1][j] += 1;
-    matrix[i + 1][j + 1] += 1;
-    matrix[i + 1][j - 1] += 1;
-    matrix[i][j + 1] += 1;
-    matrix[i][j - 1] += 1;
+    increaseRightThreeCells(matrix, i, j);
+    increaseUpAndDownCells(matrix, i, j);
   } else if (i === 15) {
-    matrix[i - 1][j] += 1;
-    matrix[i - 1][j + 1] += 1;
-    matrix[i - 1][j - 1] += 1;
-    matrix[i][j + 1] += 1;
-    matrix[i][j - 1] += 1;
+    increaseLeftThreeCells(matrix, i, j);
+    increaseUpAndDownCells(matrix, i, j);
   } else if (j === 0) {
-    matrix[i - 1][j] += 1;
-    matrix[i + 1][j] += 1;
-    matrix[i - 1][j + 1] += 1;
-    matrix[i][j + 1] += 1;
-    matrix[i + 1][j + 1] += 1;
+    increaseLeftAndRightCells(matrix, i, j);
+    increaseDownThreeCells(matrix, i, j);
   } else if (j === 15) {
-    matrix[i - 1][j] += 1;
-    matrix[i + 1][j] += 1;
-    matrix[i - 1][j - 1] += 1;
-    matrix[i][j - 1] += 1;
-    matrix[i + 1][j - 1] += 1;
+    increaseLeftAndRightCells(matrix, i, j);
+    increaseUpThreeCells(matrix, i, j);
   } else {
-    matrix[i - 1][j + 1] += 1;
-    matrix[i][j + 1] += 1;
-    matrix[i + 1][j + 1] += 1;
-    matrix[i + 1][j] += 1;
-    matrix[i - 1][j] += 1;
-    matrix[i - 1][j - 1] += 1;
-    matrix[i][j - 1] += 1;
-    matrix[i + 1][j - 1] += 1;
+    increaseLeftThreeCells(matrix, i, j);
+    increaseRightThreeCells(matrix, i, j);
+    increaseUpAndDownCells(matrix, i, j);
   }
+}
+
+function increaseRightUpTriangle(matrix: number[][], i: number, j: number) {
+  matrix[i - 1][j] += 1;
+  matrix[i][j + 1] += 1;
+  matrix[i - 1][j + 1] += 1;
+}
+function increaseRightDownTriangle(matrix: number[][], i: number, j: number) {
+  matrix[i + 1][j] += 1;
+  matrix[i][j + 1] += 1;
+  matrix[i + 1][j + 1] += 1;
+}
+
+function increaseLeftUpTriangle(matrix: number[][], i: number, j: number) {
+  matrix[i - 1][j] += 1;
+  matrix[i][j - 1] += 1;
+  matrix[i - 1][j - 1] += 1;
+}
+
+function increaseLeftDownTriangle(matrix: number[][], i: number, j: number) {
+  matrix[i + 1][j] += 1;
+  matrix[i][j - 1] += 1;
+  matrix[i + 1][j - 1] += 1;
+}
+
+function increaseRightThreeCells(matrix: number[][], i: number, j: number) {
+  matrix[i + 1][j] += 1;
+  matrix[i + 1][j + 1] += 1;
+  matrix[i + 1][j - 1] += 1;
+}
+
+function increaseLeftThreeCells(matrix: number[][], i: number, j: number) {
+  matrix[i - 1][j] += 1;
+  matrix[i - 1][j + 1] += 1;
+  matrix[i - 1][j - 1] += 1;
+}
+
+function increaseDownThreeCells(matrix: number[][], i: number, j: number) {
+  matrix[i - 1][j + 1] += 1;
+  matrix[i][j + 1] += 1;
+  matrix[i + 1][j + 1] += 1;
+}
+
+function increaseUpThreeCells(matrix: number[][], i: number, j: number) {
+  matrix[i - 1][j - 1] += 1;
+  matrix[i][j - 1] += 1;
+  matrix[i + 1][j - 1] += 1;
+}
+
+function increaseUpAndDownCells(matrix: number[][], i: number, j: number) {
+  matrix[i][j + 1] += 1;
+  matrix[i][j - 1] += 1;
+}
+function increaseLeftAndRightCells(matrix: number[][], i: number, j: number) {
+  matrix[i - 1][j] += 1;
+  matrix[i + 1][j] += 1;
 }
